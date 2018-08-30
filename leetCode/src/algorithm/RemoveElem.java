2feed2b56937e597eabe23c2a9b50d7d9983d1b4
hui10.yang <hui10.yang@vipshop.com>
2018-08-30 15:55:30 +0800

    流处理计算移除元素后数组长度

--- leetCode/src/algorithm/RemoveElem.java
+++ leetCode/src/algorithm/RemoveElem.java
@@ -1,7 +1,17 @@
 package algorithm;
 
+import java.util.stream.IntStream;
+
 /**
  * Created by hui10.yang on 18/8/30.
  */
 public class RemoveElem {
+    public static void main(String[] args) {
+        int[] a = new int[]{0,1,2,2,3, 0, 4, 2};
+        System.out.println(removeElemFunction(a));
+    }
+
+    private static long removeElemFunction(int[] a) {
+        return IntStream.of(a).distinct().count();
+    }
 }
