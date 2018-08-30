6103b4e053ced3d49be6d3cc8fd09f38f2b82f84
hui10.yang <hui10.yang@vipshop.com>
2018-08-30 17:56:45 +0800

    循环语句移除元素

--- leetCode/src/algorithm/RemoveElem.java
+++ leetCode/src/algorithm/RemoveElem.java
@@ -8,10 +8,28 @@ import java.util.stream.IntStream;
 public class RemoveElem {
     public static void main(String[] args) {
         int[] a = new int[]{0,1,2,2,3, 0, 4, 2};
-        System.out.println(removeElemFunction(a));
+        System.out.println(removeElemFunction(a,2));
+        System.out.println(removeElem(a,2));
     }
 
-    private static long removeElemFunction(int[] a) {
-        return IntStream.of(a).distinct().count();
+    /**
+     * 移除元素
+     * @param a
+     * @return
+     */
+    private static long removeElemFunction(int[] a,int var) {
+        return IntStream.of(a).filter(n -> n != var).count();
     }
+
+    private static long removeElem(int[] b,int var) {
+        long length=0;
+        for (int i = 0; i < b.length; i++) {
+            if (b[i] == var) {
+                continue;
+            }
+            length++;
+        }
+        return length;
+    }
+
 }
